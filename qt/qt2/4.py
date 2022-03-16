2eb2344d668e0386370128d5282c75ec1f881c26
Roman M. Gerasimov <romagrizly@gmail.com>
2022-03-16 11:17:48 +0500

    text browser

--- qt/qt2/4.py
+++ qt/qt2/4.py
@@ -1,8 +1,8 @@
 import sys
 
 # Импортируем из PyQt5.QtWidgets классы для создания приложения и виджета
-from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow
-from ui_calc_two_number import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow, QFileDialog
+from ui_file_browser import Ui_MainWindow
 from PyQt5.uic import loadUi
 
 
@@ -11,6 +11,27 @@ class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.file_name = None
+        self.pushButton_load.clicked.connect(self.load_action)
+        self.pushButton_save.clicked.connect(self.save_action)
+
+    def load_action(self):
+        file_name = QFileDialog.getOpenFileName(self, 'Open file')[0]
+        if not file_name:
+            return
+
+        self.file_name = file_name
+        self.label_file_name.setText(file_name)
+        with open(file_name, "r", encoding="utf-8") as input_file:
+            data = input_file.read()
+            self.textEdit.setText(data)
+
+    def save_action(self):
+        if not self.file_name:
+            return
+        with open(self.file_name, "w") as output_file:
+            data = self.textEdit.toPlainText()
+            output_file.write(data)
 
 
 def except_hook(cls, exception, traceback):
@@ -18,7 +39,6 @@ def except_hook(cls, exception, traceback):
 
 
 sys.excepthook = except_hook
-
 
 if __name__ == '__main__':
     # Создадим класс приложения PyQT
